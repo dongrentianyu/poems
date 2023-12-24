96cf8c5b33f37d1144e5e2a7e23de2a6e4b5c224
dongrentianyu <3465645884@qq.com>
2023-12-24 21:26:03 +0800

    Update rspress.config.ts

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
+  base: "/poems",
   root: path.join(__dirname, 'docs'),
   title: 'Poems',
   description: 'Poems for life',
